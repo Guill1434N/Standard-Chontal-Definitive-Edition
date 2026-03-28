edf246a3e8f550e860857b53d0108b90bb75937b
Guille Pao <zeledonpaoj@gmail.com>
2026-03-28 13:34:37 -0600

    Primer Commit del proyecto

---
+++ Home.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.panelStatus = new System.Windows.Forms.Panel();
+            this.panelEscritorio = new System.Windows.Forms.Panel();
+            this.msPrincipal = new System.Windows.Forms.MenuStrip();
+            this.entradaDeDatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestionDeUsuariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestionDeProductosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestionDeProveedoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.productosSinStockToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnVentas = new System.Windows.Forms.Button();
+            this.btnCompras = new System.Windows.Forms.Button();
+            this.btnDevolucion = new System.Windows.Forms.Button();
+            this.procesosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelHeader.SuspendLayout();
+            this.panelEscritorio.SuspendLayout();
+            this.msPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(41)))), ((int)(((byte)(72)))));
+            this.panelHeader.Controls.Add(this.msPrincipal);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(1264, 60);
+            this.panelHeader.TabIndex = 0;
+            // 
+            // panelStatus
+            // 
+            this.panelStatus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(41)))), ((int)(((byte)(72)))));
+            this.panelStatus.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelStatus.Location = new System.Drawing.Point(0, 714);
+            this.panelStatus.Name = "panelStatus";
+            this.panelStatus.Size = new System.Drawing.Size(1264, 35);
+            this.panelStatus.TabIndex = 1;
+            this.panelStatus.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panelEscritorio
+            // 
+            this.panelEscritorio.BackColor = System.Drawing.Color.Transparent;
+            this.panelEscritorio.Controls.Add(this.btnDevolucion);
+            this.panelEscritorio.Controls.Add(this.btnCompras);
+            this.panelEscritorio.Controls.Add(this.btnVentas);
+            this.panelEscritorio.Controls.Add(this.label2);
+            this.panelEscritorio.Controls.Add(this.label1);
+            this.panelEscritorio.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelEscritorio.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panelEscritorio.ForeColor = System.Drawing.Color.White;
+            this.panelEscritorio.Location = new System.Drawing.Point(0, 60);
+            this.panelEscritorio.Name = "panelEscritorio";
+            this.panelEscritorio.Size = new System.Drawing.Size(1264, 654);
+            this.panelEscritorio.TabIndex = 2;
+            // 
+            // msPrincipal
+            // 
+            this.msPrincipal.BackColor = System.Drawing.Color.Transparent;
+            this.msPrincipal.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.msPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.entradaDeDatosToolStripMenuItem,
+            this.procesosToolStripMenuItem,
+            this.reportesToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.msPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.msPrincipal.Name = "msPrincipal";
+            this.msPrincipal.Size = new System.Drawing.Size(1264, 27);
+            this.msPrincipal.TabIndex = 0;
+            this.msPrincipal.Text = "menuStrip1";
+            // 
+            // entradaDeDatosToolStripMenuItem
+            // 
+            this.entradaDeDatosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gestionDeUsuariosToolStripMenuItem,
+            this.gestionDeProductosToolStripMenuItem,
+            this.gestionDeProveedoresToolStripMenuItem});
+            this.entradaDeDatosToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Control;
+            this.entradaDeDatosToolStripMenuItem.Name = "entradaDeDatosToolStripMenuItem";
+            this.entradaDeDatosToolStripMenuItem.Size = new System.Drawing.Size(125, 23);
+            this.entradaDeDatosToolStripMenuItem.Text = "Entrada de datos";
+            // 
+            // reportesToolStripMenuItem
+            // 
+            this.reportesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.productosSinStockToolStripMenuItem});
+            this.reportesToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reportesToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Control;
+            this.reportesToolStripMenuItem.Name = "reportesToolStripMenuItem";
+            this.reportesToolStripMenuItem.Size = new System.Drawing.Size(73, 23);
+            this.reportesToolStripMenuItem.Text = "Reportes";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Control;
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(46, 23);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // gestionDeUsuariosToolStripMenuItem
+            // 
+            this.gestionDeUsuariosToolStripMenuItem.Name = "gestionDeUsuariosToolStripMenuItem";
+            this.gestionDeUsuariosToolStripMenuItem.Size = new System.Drawing.Size(224, 24);
+            this.gestionDeUsuariosToolStripMenuItem.Text = "Gestion de Usuarios";
+            // 
+            // gestionDeProductosToolStripMenuItem
+            // 
+            this.gestionDeProductosToolStripMenuItem.Name = "gestionDeProductosToolStripMenuItem";
+            this.gestionDeProductosToolStripMenuItem.Size = new System.Drawing.Size(224, 24);
+            this.gestionDeProductosToolStripMenuItem.Text = "Gestion de Productos";
+            // 
+            // gestionDeProveedoresToolStripMenuItem
+            // 
+            this.gestionDeProveedoresToolStripMenuItem.Name = "gestionDeProveedoresToolStripMenuItem";
+            this.gestionDeProveedoresToolStripMenuItem.Size = new System.Drawing.Size(224, 24);
+            this.gestionDeProveedoresToolStripMenuItem.Text = "Gestion de Proveedores";
+            // 
+            // productosSinStockToolStripMenuItem
+            // 
+            this.productosSinStockToolStripMenuItem.Name = "productosSinStockToolStripMenuItem";
+            this.productosSinStockToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.productosSinStockToolStripMenuItem.Text = "Productos sin Stock";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 26F);
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(302, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(713, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bienvenido Administrador Standard Chontal ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label2.ForeColor = System.Drawing.Color.Navy;
+            this.label2.Location = new System.Drawing.Point(467, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(375, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sistema de Gestión General Tienda Standard Chontal";
+            // 
+            // btnVentas
+            // 
+            this.btnVentas.ForeColor = System.Drawing.Color.Black;
+            this.btnVentas.Location = new System.Drawing.Point(471, 201);
+            this.btnVentas.Name = "btnVentas";
+            this.btnVentas.Size = new System.Drawing.Size(75, 23);
+            this.btnVentas.TabIndex = 2;
+            this.btnVentas.Text = "Ventas";
+            this.btnVentas.UseVisualStyleBackColor = true;
+            // 
+            // btnCompras
+            // 
+            this.btnCompras.ForeColor = System.Drawing.Color.Black;
+            this.btnCompras.Location = new System.Drawing.Point(573, 201);
+            this.btnCompras.Name = "btnCompras";
+            this.btnCompras.Size = new System.Drawing.Size(75, 23);
+            this.btnCompras.TabIndex = 3;
+            this.btnCompras.Text = "Compras";
+            this.btnCompras.UseVisualStyleBackColor = true;
+            // 
+            // btnDevolucion
+            // 
+            this.btnDevolucion.ForeColor = System.Drawing.Color.Black;
+            this.btnDevolucion.Location = new System.Drawing.Point(686, 201);
+            this.btnDevolucion.Name = "btnDevolucion";
+            this.btnDevolucion.Size = new System.Drawing.Size(99, 23);
+            this.btnDevolucion.TabIndex = 4;
+            this.btnDevolucion.Text = "Devoluciones";
+            this.btnDevolucion.UseVisualStyleBackColor = true;
+            this.btnDevolucion.Click += new System.EventHandler(this.btnDevolucion_Click);
+            // 
+            // procesosToolStripMenuItem
+            // 
+            this.procesosToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Control;
+            this.procesosToolStripMenuItem.Name = "procesosToolStripMenuItem";
+            this.procesosToolStripMenuItem.Size = new System.Drawing.Size(75, 23);
+            this.procesosToolStripMenuItem.Text = "Procesos";
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 749);
+            this.Controls.Add(this.panelEscritorio);
+            this.Controls.Add(this.panelStatus);
+            this.Controls.Add(this.panelHeader);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.msPrincipal;
+            this.Name = "Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Standard Chontal Matagalpa - Sistema de Gestión";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            this.panelEscritorio.ResumeLayout(false);
+            this.panelEscritorio.PerformLayout();
+            this.msPrincipal.ResumeLayout(false);
+            this.msPrincipal.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Panel panelStatus;
+        private System.Windows.Forms.Panel panelEscritorio;
+        private System.Windows.Forms.MenuStrip msPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem entradaDeDatosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestionDeUsuariosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestionDeProductosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestionDeProveedoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem productosSinStockToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDevolucion;
+        private System.Windows.Forms.Button btnCompras;
+        private System.Windows.Forms.Button btnVentas;
+        private System.Windows.Forms.ToolStripMenuItem procesosToolStripMenuItem;
+    }
+}
+

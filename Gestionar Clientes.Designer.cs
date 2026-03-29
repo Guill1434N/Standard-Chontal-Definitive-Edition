a8d47706372965994d42d71dfebc57f5748967ae
Haxll <haxeljosuetalavera029@gmail.com>
2026-03-28 22:51:51 -0600

    configuracion de columnas y estilos del datagriew de clientes

--- Gestionar Clientes.Designer.cs
+++ Gestionar Clientes.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.txtNombre = new System.Windows.Forms.Label();
-            this.txtTelefono = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.LabelTelefono = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.cmbCategoria = new System.Windows.Forms.ComboBox();
             this.btnGuardar = new System.Windows.Forms.Button();
@@ -40,6 +40,8 @@
             this.colCelular = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colTipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colFecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,23 +55,23 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Panel de Gestion de Clientes";
             // 
-            // txtNombre
+            // label
             // 
-            this.txtNombre.AutoSize = true;
-            this.txtNombre.Location = new System.Drawing.Point(40, 69);
-            this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(81, 28);
-            this.txtNombre.TabIndex = 1;
-            this.txtNombre.Text = "Nombre";
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(40, 69);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(81, 28);
+            this.label.TabIndex = 1;
+            this.label.Text = "Nombre";
             // 
-            // txtTelefono
+            // LabelTelefono
             // 
-            this.txtTelefono.AutoSize = true;
-            this.txtTelefono.Location = new System.Drawing.Point(39, 113);
-            this.txtTelefono.Name = "txtTelefono";
-            this.txtTelefono.Size = new System.Drawing.Size(82, 28);
-            this.txtTelefono.TabIndex = 2;
-            this.txtTelefono.Text = "Telefono";
+            this.LabelTelefono.AutoSize = true;
+            this.LabelTelefono.Location = new System.Drawing.Point(39, 113);
+            this.LabelTelefono.Name = "LabelTelefono";
+            this.LabelTelefono.Size = new System.Drawing.Size(82, 28);
+            this.LabelTelefono.TabIndex = 2;
+            this.LabelTelefono.Text = "Telefono";
             // 
             // label2
             // 
@@ -102,6 +104,7 @@
             this.btnGuardar.TabIndex = 5;
             this.btnGuardar.Text = "Registrar Cliente";
             this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
             // btnLimpiar
             // 
@@ -147,19 +150,35 @@
             this.colFecha.HeaderText = "Fecha";
             this.colFecha.Name = "colFecha";
             // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(127, 69);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 34);
+            this.txtNombre.TabIndex = 8;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(139, 120);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(100, 34);
+            this.txtTelefono.TabIndex = 9;
+            // 
             // Gestionar_Clientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.HighlightText;
             this.ClientSize = new System.Drawing.Size(1333, 749);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.dgvClientes);
             this.Controls.Add(this.btnLimpiar);
             this.Controls.Add(this.btnGuardar);
             this.Controls.Add(this.cmbCategoria);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtTelefono);
-            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.LabelTelefono);
+            this.Controls.Add(this.label);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Segoe UI Light", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
@@ -174,8 +193,8 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label txtNombre;
-        private System.Windows.Forms.Label txtTelefono;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label LabelTelefono;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox cmbCategoria;
         private System.Windows.Forms.Button btnGuardar;
@@ -185,5 +204,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn colCelular;
         private System.Windows.Forms.DataGridViewTextBoxColumn colTipo;
         private System.Windows.Forms.DataGridViewTextBoxColumn colFecha;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtTelefono;
     }
 }
--- Gestionar Clientes.cs
+++ Gestionar Clientes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,5 +22,41 @@ namespace WindowsFormsApp1
         {
 
         }
-    }
-}
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            // validamos que no este vacio el nommbre (minimo esto)
+            if ((label.Text.Trim() == ""))
+            {
+                MessageBox.Show("El nombre no puede ir vacio");
+                label.Focus();
+                return;
+
+            }
+
+            //Agregamos los datos de la tabla
+            dgvClientes.Rows.Add(label.Text.Trim(), LabelTelefono.Text.Trim(), cmbCategoria.Text, DateTime.Now.ToString("add/MM/yyyy"));
+
+            //Limpiar para el siguiente cliente
+            LimpiarFormulario();
+
+            //Mensaje de exito 
+            MessageBox.Show("Cliente registrado con exito", "sistema");
+        }
+            
+            //creamos  este metodo aparte que se limpie el codigo
+            private void LimpiarFormulario()
+            {
+                txtNombre.Clear();
+                txtTelefono.Clear();
+                cmbCategoria.SelectedIndex = -1;
+
+
+            }
+
+            
+                
+            }
+        }
+    
+

5fdb23e10b274709b12cd7fc760170eaaa37f6f5
Bcnokarun <bryansteven377@gmail.com>
2026-04-18 12:19:24 -0600

    Mejora del diseno visual

--- Nuevo Producto.cs
+++ Nuevo Producto.cs
@@ -1,14 +1,5 @@
 ﻿using System;
-using System.Windows.Forms;
-
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
@@ -19,39 +10,88 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
 
-            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
-            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            btnGuardar.Click += btnGuardar_Click;
+            btnCancelar.Click += btnCancelar_Click;
 
-            if (cmbCategoria.Items.Count > 0) cmbCategoria.SelectedIndex = 0;
-            if (cmbUnidadMedida.Items.Count > 0) cmbUnidadMedida.SelectedIndex = 0;
+            if (cmbCategoria.Items.Count > 0)
+                cmbCategoria.SelectedIndex = 0;
+
+            if (cmbUnidadMedida.Items.Count > 0)
+                cmbUnidadMedida.SelectedIndex = 0;
+
+            this.Paint += Nuevo_Producto_Paint;
+        }
+
+        private void Nuevo_Producto_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(236, 239, 243);
+            this.StartPosition = FormStartPosition.CenterScreen;
         }
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(txtCodigo.Text) || string.IsNullOrWhiteSpace(txtNombre.Text))
+            if (string.IsNullOrWhiteSpace(txtCodigo.Text) ||
+               string.IsNullOrWhiteSpace(txtNombre.Text))
             {
-                MessageBox.Show("Por favor, complete el Código y el Nombre del producto.", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show("Complete Código y Nombre");
                 return;
             }
 
-            if (numPrecioVenta.Value < numPrecioCompra.Value)
-            {
-                DialogResult resp = MessageBox.Show("El precio de venta es menor al precio de compra. ¿Desea continuar de todos modos?",
-                                                   "Confirmación de margen", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                if (resp == DialogResult.No) return;
-            }
-
-            MessageBox.Show("Producto guardado exitosamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-            this.Close();
+            MessageBox.Show("Producto guardado correctamente");
+            Close();
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Close();
         }
 
-        private void lblNombre_Click(object sender, EventArgs e) { }
-        private void lblCodigo_Click(object sender, EventArgs e) { }
+        private void Nuevo_Producto_Paint(object sender, PaintEventArgs e)
+        {
+            using (Pen azul = new Pen(Color.FromArgb(0, 83, 187), 4))
+            using (Pen rojo = new Pen(Color.FromArgb(220, 40, 40), 2))
+            {
+                e.Graphics.DrawLine(azul, 0, 0, this.Width, 0);
+                e.Graphics.DrawLine(rojo, 0, 5, this.Width, 5);
+
+                e.Graphics.DrawLine(azul, 0, this.Height - 8, this.Width, this.Height - 8);
+                e.Graphics.DrawLine(rojo, 0, this.Height - 3, this.Width, this.Height - 3);
+            }
+        }
+
+        private void panelLogo_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panelContenedor_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void lblNombre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblUnidadMedida_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblStockMinimo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

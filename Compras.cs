cfe287783cf00ec75a65abc105f35a38e536a5ee
Crislex <crislexanderpruiz123@gmail.com>
2026-05-08 14:22:18 -0600

    Agregar nuevo form de compras

--- Compras.cs
+++ Compras.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    internal class Compras
+    public partial class Compras: Form
     {
+        public Compras()
+        {
+            InitializeComponent();
+        }
 
+        private void Compras_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
